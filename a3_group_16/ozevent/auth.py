677e5755a1312805f28df6c86f530d4864522796
Quinn Rigby <qarigby@gmail.com>
2025-05-23 11:03:37 +1000

    implemented login and register functionality

--- a3_group_16/ozevent/auth.py
+++ a3_group_16/ozevent/auth.py
@@ -15,20 +15,65 @@ def login():
     login_form = LoginForm()
     error = None
     if login_form.validate_on_submit():
-        user_name = login_form.user_name.data
+        user_name = login_form.username.data
         password = login_form.password.data
-        user = db.session.scalar(db.select(User).where(User.name==user_name))
+        user = db.session.scalar(db.select(User).where(User.username==user_name))
         if user is None:
-            error = 'Incorrect user name'
+            error = 'That username doesnt exist, please try again'
         elif not check_password_hash(user.password_hash, password): # takes the hash and cleartext password
-            error = 'Incorrect password'
+            error = 'Incorrect password, please try again'
         if error is None:
             login_user(user)
             nextp = request.args.get('next') # this gives the url from where the login page was accessed
             print(nextp)
-            if next is None or not nextp.startswith('/'):
-                return redirect(url_for('index'))
+            if nextp is None or not nextp.startswith('/'):
+                return redirect(url_for('main.index'))
             return redirect(nextp)
         else:
             flash(error)
     return render_template('user.html', form=login_form, heading='Login')
+
+
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    register = RegisterForm()
+    #the validation of form is fine, HTTP request is POST
+    if (register.validate_on_submit()==True):
+            #get username, password and email from the form
+            uname = register.username.data
+            pwd = register.password.data
+            email = register.email.data
+            name = register.name.data
+            
+            #check if a user exists
+            user = db.session.scalar(db.select(User).where(User.username==uname))
+            email_exists = db.session.scalar(db.select(User).where(User.email==email))
+
+            if user:#this returns true when user is not None
+                flash('Username already exists, please try another')
+                return redirect(url_for('auth.register'))
+            
+            # If ther email already exists in the database
+            elif email_exists:
+                flash('A user with that email already exists, please use another email, or log-in to an existing account.') 
+                return redirect(url_for('auth.register'))
+            
+
+            # don't store the password in plaintext!
+            pwd_hash = generate_password_hash(pwd)
+            #create a new User model object
+            new_user = User(name=name, username=uname, password_hash=pwd_hash, email=email)
+            db.session.add(new_user)
+            db.session.commit()
+            #commit to the database and redirect to HTML page
+            return redirect(url_for('auth.login'))
+    #the else is called when the HTTP request calling this page is a GET
+    else:
+        return render_template('user.html', form=register, heading='Register')
+    
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
