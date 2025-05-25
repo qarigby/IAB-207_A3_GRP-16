6c5336b4a1ce41cf214ff686335edffc45247ea3
Quinn Rigby <qarigby@gmail.com>
2025-05-25 22:54:14 +1000

    Implemented form creation

--- a3_group_16/ozevent/events.py
+++ a3_group_16/ozevent/events.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required
 from .models import Event
+from .forms import EventForm
+from .utils import check_upload_file
 from . import db 
 
 events_bp = Blueprint('events', __name__, url_prefix='/events')
@@ -9,7 +11,47 @@ events_bp = Blueprint('events', __name__, url_prefix='/events')
 def show():
     return render_template('events/show.html')
 
-@events_bp.route('/create')
+@events_bp.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
-    return render_template('events/create.html')
+    form = EventForm()
+
+    if form.validate_on_submit():
+        # storing image filepath
+        db_fp = check_upload_file(form)
+
+        # Assigning database object fields
+        event = Event(
+            name=form.name.data,
+            artist=form.artist.data,
+            genre=form.genre.data,
+            venue=form.venue.data,
+            date=form.date.data,
+            start_time=form.start_time.data,
+            end_time=form.end_time.data,
+            available_tickets=form.available_tickets.data,
+            short_description=form.short_description.data,
+            description=form.description.data,
+            image=db_fp
+        )
+
+        # Add the object to the db session
+        db.session.add(event)
+        # Commit to the database
+        db.session.commit()
+
+        # Alert the user upon success
+        flash('Successfully created new event', 'success')
+        return redirect(url_for('main.index'))   
+    if form.errors:
+        all_errors = ", ".join(
+            err_msg
+            for field_errors in form.errors.values()
+            for err_msg in field_errors
+        )
+        flash(f"Cannot create event: {all_errors}")
+
+
+    return render_template('events/create.html', form=form)
+
+# Check over but i think this is done
--- a3_group_16/ozevent/forms.py
+++ a3_group_16/ozevent/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from wtforms import SelectField
 from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, DateField, TimeField, IntegerField
 from wtforms.validators import InputRequired, Length, Email, EqualTo, ValidationError, NumberRange
 from flask_wtf.file import FileRequired, FileField, FileAllowed
@@ -48,26 +49,41 @@ class CommentForm(FlaskForm):
     text = TextAreaField('Comment', [InputRequired('Please enter your comment')])
     submit = SubmitField('Post')
 
+
 # Event Creation Form
+GENRE_CHOICES = [
+    ('', 'Select a genre'),  # Placeholder
+    ('rock', 'Rock'),
+    ('hip-hop', 'Hip-Hop'),
+    ('pop', 'Pop'),
+    ('edm', 'EDM'),
+    ('r&b', 'R & B'),
+    ('latin', 'Latin'),
+    ('k-pop', 'K-Pop'),
+    ('country', 'Country'),
+    ('jazz', 'Jazz'),
+    ('classical', 'Classical')
+]
+
 class EventForm(FlaskForm):
     name = StringField("Event Name", validators=[InputRequired('Please enter the event name')])
     artist = StringField("Artist Name(s)", validators=[InputRequired('Please enter the artist name(s)')])
-    genre = StringField('Genre', validators=[InputRequired('Please enter a genre')])
+    genre = SelectField('Genre', choices=GENRE_CHOICES, validators=[InputRequired(message='Please select a genre')])
     venue = StringField('Venue', validators=[InputRequired('Please enter a venue')])
     date = DateField('Date', format='%Y-%m-%d', validators=[InputRequired('Please enter a date')])
     start_time = TimeField('Start Time', format='%H:%M', validators=[InputRequired('Please enter a start time')])
     end_time = TimeField('End Time', format='%H:%M', validators=[InputRequired('Please enter an end time')])
     available_tickets = IntegerField('Number of Available Tickets', validators=[InputRequired('Please enter the number of available tickets'), NumberRange(min=1, message="Quantity must be greater than 1.")])
-    short_description = StringField('Short Description', validators=[InputRequired('Please enter a short description'), Length(max=255, message="Must be less than 255 characters long")])
+    short_description = TextAreaField('Short Description', validators=[InputRequired('Please enter a short description'), Length(max=255, message="Must be less than 255 characters long")])
     description = TextAreaField('Description', validators=[InputRequired('Please enter a description of the event')])
     image = FileField('Cover Image', validators=[FileRequired(message='An image must be uploaded'), FileAllowed(ALLOWED_FILES, message='Only PNG or JPG files allowed')])
-
-
+    submit = SubmitField('Create')
+    
     def validate_date(self, field):
         if field.data < date.today():
             raise ValidationError("Date cannot be in the past")
         
-    def validate(self):
+    def validate(self, extra_validators=None):
         # Run default validations first
         if not super().validate():
             return False
