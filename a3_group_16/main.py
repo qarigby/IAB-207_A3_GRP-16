bb334ffee148e6633a6afd42ce81879ee35df915
Hamish Renwick <n10170162@qut.edu.au>
2025-06-14 18:53:32 +1000

    Commenting

--- a3_group_16/main.py
+++ a3_group_16/main.py
@@ -1,6 +1,6 @@
 from ozevent import create_app
 
-# Initialise Web App
+# Initialise/Run Web App
 if __name__ == '__main__':
     app = create_app()
     app.run(debug=True) # Set false in deployment
