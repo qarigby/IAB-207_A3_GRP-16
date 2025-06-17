84968c0f68f2bbbe4e8090064b8aee5292f149ca
Quinn Rigby <qarigby@gmail.com>
2025-06-17 22:00:52 +1000

    booking cancellation

--- a3_group_16/ozevent/bookings.py
+++ a3_group_16/ozevent/bookings.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, flash, render_template, request, redirect, url_for
+from flask import Blueprint, abort, flash, render_template, request, redirect, url_for
 from flask_login import current_user, login_required
-from .models import Booking
+from .models import Booking, Event
 from . import db 
 
 booking_bp = Blueprint('booking', __name__)
@@ -11,5 +11,36 @@ booking_bp = Blueprint('booking', __name__)
 def bookings():
     bookings = db.session.scalars(db.select(Booking).where(Booking.user_id == current_user.id)).all()
     return render_template('booking_history.html', bookings=bookings)
+
+# Cancelling a booking
+@booking_bp.route('/bookings/cancel-<booking_id>')
+@login_required
+def cancel(booking_id):
+    booking = db.session.scalar(db.select(Booking).where(Booking.id == booking_id))
+    event = db.session.scalar(db.select(Event).where(Event.id == booking.event.id))
+
+    # if the booking doesnt exist
+    if not booking:
+        abort(404)
+    # If the user does not own the booking
+    if booking.user_id != current_user.id:
+        abort(403)
+
+    # Add the number of tickets being cancelled back to the event
+    event.available_tickets = event.available_tickets + booking.num_tickets
+
+    # If the event was sold out - it now isnt 
+    if event.status == "Sold Out":
+        event.status == "Open"
     
+    # if the event is open (can't cancel bookings for inactive or cancelled events)
+    if event.status == 'Open':
+        db.session.delete(booking) #delete the booking from db
+        db.session.commit()
+        flash('Booking cancelled successfully.')
+    else:
+        flash('Unable to cancel booking. The event is either in the past or has previously been cancelled.')
+    return redirect(url_for('booking.bookings', event_id=event.id))
+
     
+    # Still need to add filtering so users can view open, inactive, and cancelled events by status
